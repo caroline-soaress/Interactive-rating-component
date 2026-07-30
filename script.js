9654d02720fe58577576d0bb6f81fc2d3b5e4cfb
caroline-soaress <caroline.soares@sptech.school>
2026-07-30 16:31:04 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+let notaSelecionada;
+
+function calcular(botao) {
+  const numero = botao.textContent;
+  notaSelecionada = numero;
+}
+
+function submit() {
+  // Verifica se a notaSelecionada ainda está sem valor (undefined)
+  if (!notaSelecionada) {
+    alert("Please select a rating before submitting.");
+    return; // Para a execução da função aqui
+  }
+
+  const avaliationDiv = document.querySelector(".avaliation");
+  const tankyouDiv = document.querySelector(".tankyou");
+
+  // Adiciona a "capa de invisibilidade" na tela de avaliação
+  avaliationDiv.classList.add("hidden");
+  // Remove a "capa de invisibilidade" da tela de "Obrigado"
+  tankyouDiv.classList.remove("hidden");
+
+  document.querySelector(".number-tank").textContent = notaSelecionada;
+}
